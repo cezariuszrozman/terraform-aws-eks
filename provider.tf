2005a5fe9f0d8c2c1f813f0446501352d958850e
Vrezh Kocharyan <vrezh.kocharyan@pl.abb.com>
2025-02-18 16:25:04 +0100

    update

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = local.region
 }
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.0-pre1"
+    }
+  }
+}
+
+provider "helm" {
+  # Configuration options
+}
