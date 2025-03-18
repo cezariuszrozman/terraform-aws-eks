1210cc1cc0ef836563cc7c7d8ea7d681ef338a87
Vrezh Kocharyan <vrezh.kocharyan@pl.abb.com>
2025-03-18 20:53:04 +0100

    add external-dns deployment with IAM-role

---
+++ external-dns.tf
@@ -0,0 +1,76 @@
+# IAM Role for External-DNS
+module "external_dns_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                     = "external-dns"
+  attach_external_dns_policy    = true
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/*"]
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks_al2.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+
+  tags = local.tags
+}
+
+# Helm release for External-DNS
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  namespace  = "kube-system"
+  version    = "1.13.1"
+
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "aws.zoneType"
+    value = "public"
+  }
+
+  set {
+    name  = "domainFilters[0]"
+    value = "szkolenie.com"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
+  }
+
+  set {
+    name  = "registry"
+    value = "txt"
+  }
+
+  set {
+    name  = "txtOwnerId"
+    value = module.eks_al2.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.external_dns_role.iam_role_arn
+  }
+
+  depends_on = [
+    module.eks_al2,
+    aws_route53_zone.primary
+  ]
+}
