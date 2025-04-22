2a2b8fcbd3f6f1361dd3659b84f20c9966e129d3
Bartosz Urbaniak <burbaniak@rxsense.com>
2025-04-22 22:26:05 +0200

    Tag subnets

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,16 @@
-resource "aws_ec2_tag" "private_subnet_tags" {
+resource "aws_ec2_tag" "cluster_tag_private_subnets" {
   for_each = toset(var.private_subnets)
 
   resource_id = each.value
+  key         = "kubernetes.io/cluster/${local.name}-al2"
+  value       = "shared"
+}
 
-  tags = {
-    "kubernetes.io/cluster/${local.name}-al2" = "shared"
-    "kubernetes.io/role/internal-elb"         = "1"
-  }
+# 2nd tag: kubernetes.io/role/internal-elb = 1
+resource "aws_ec2_tag" "role_tag_private_subnets" {
+  for_each = toset(var.private_subnets)
+
+  resource_id = each.value
+  key         = "kubernetes.io/role/internal-elb"
+  value       = "1"
 }
